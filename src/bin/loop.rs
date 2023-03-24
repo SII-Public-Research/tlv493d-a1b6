254dbbaf882ff3f1314cd955fd22205219a89fee
Eldred HABERT <eldred.habert@sii.fr>
2023-03-24 11:44:26 +0100

    Run rustfmt

--- src/bin/loop.rs
+++ src/bin/loop.rs
@@ -3,10 +3,10 @@ use tlv493d_a1b6_raspberry::{Tlv493d, *};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Create MPU
-    let mut i2c   = I2c::new().expect("1");
-    let addr      =  ADDRESS_BASE_1;
-    let mode      = Mode::Master;
-    let mut tlv   = Tlv493d::new(i2c, addr, &mode).unwrap();
+    let mut i2c = I2c::new().expect("1");
+    let addr = ADDRESS_BASE_1;
+    let mode = Mode::Master;
+    let mut tlv = Tlv493d::new(i2c, addr, &mode).unwrap();
     let mut delay = rppal::hal::Delay::new();
 
     //Initialize the variable which will contain data
@@ -17,18 +17,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     loop {
         // get 3d hall data (computing means and standard-deviations over N measurements) : bx, by, bz
-        match tlv.get_b_mean(n, &mut delay) 
-        {
-            Ok(b) => 
-            {   println!("Bx={:.1}mT By={:.1}mT Bz={:.1}mT",b.bx, b.by, b.bz);
+        match tlv.get_b_mean(n, &mut delay) {
+            Ok(b) => {
+                println!("Bx={:.1}mT By={:.1}mT Bz={:.1}mT", b.bx, b.by, b.bz);
                 _bfield = b;
             }
 
-            Err(error) => 
-            {   println!("Erreur : {0:?}", error);
-                match error 
-                {   Error::AdcLockup => 
-                    {   i2c = I2c::new().expect("1");
+            Err(error) => {
+                println!("Erreur : {0:?}", error);
+                match error {
+                    Error::AdcLockup => {
+                        i2c = I2c::new().expect("1");
                         tlv = Tlv493d::new(i2c, addr, &mode).unwrap();
                     }
                     _other => {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-use core::marker::PhantomData;
-use log::{debug};
 use bitflags::bitflags;
-use embedded_hal::blocking::{i2c, delay::*};
+use core::marker::PhantomData;
+use embedded_hal::blocking::{delay::*, i2c};
+use log::debug;
 
 #[cfg(feature = "std")]
 extern crate std;
-
 
 // The crate is initially made by Ryankurte.
 // Changed some small things because it was not working for me (the initialisation of the sensor, with the address setting up, ...)
@@ -19,8 +18,6 @@ extern crate std;
 // because of this, unless the crate is adapted by someone.
 //
 // /!\ During tests, Master mode was working well, but not the others ones...
-
-
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 //       CRATE "sensor_tlv493d" BY RYANKURTE, with small changes (more reliable for my use)
@@ -37,18 +34,19 @@ pub struct Tlv493d<I2c, E> {
 // Base address for Tlv493d, bottom bit set during power up
 // based on value of SDA.
 pub const ADDRESS_BASE_1: u8 = 0b1011110;
-pub const ADDRESS_BASE_2: u8 = 0b0011111; /// Added ///
+pub const ADDRESS_BASE_2: u8 = 0b0011111;
+/// Added ///
 
 // Read registers for the Tlv493d
 pub enum ReadRegisters {
-    Rx      = 0x00, // X direction flux (Bx[11..4])
-    By      = 0x01, // Y direction flux (By[11..4])
-    Bz      = 0x02, // X direction flux (Bz[11..4])
-    Temp    = 0x03, // Temperature high bits, frame counter, channel, (Temp[11..8] | FRM[1..0] | CH[1..0])
-    Bx2     = 0x04, // Lower X and Y flux (Bx[3..0] | Bx[3..0])
-    Bz2     = 0x05, // Flags + Lower Z flux (Reserved | T | FF | PD | Bz[3..0])
-    Temp2   = 0x06, // Temperature low bits (Temp[7..0])
-    
+    Rx = 0x00,    // X direction flux (Bx[11..4])
+    By = 0x01,    // Y direction flux (By[11..4])
+    Bz = 0x02,    // X direction flux (Bz[11..4])
+    Temp = 0x03, // Temperature high bits, frame counter, channel, (Temp[11..8] | FRM[1..0] | CH[1..0])
+    Bx2 = 0x04,  // Lower X and Y flux (Bx[3..0] | Bx[3..0])
+    Bz2 = 0x05,  // Flags + Lower Z flux (Reserved | T | FF | PD | Bz[3..0])
+    Temp2 = 0x06, // Temperature low bits (Temp[7..0])
+
     FactSet1 = 0x07,
     FactSet2 = 0x08,
     FactSet3 = 0x09,
@@ -56,29 +54,29 @@ pub enum ReadRegisters {
 
 // Write registers for the Tlv493d
 pub enum WriteRegisters {
-    Res     = 0x00, // Reserved
-    Mode1   = 0x01, // Mode 1 register (P | IICAddr[1..0] | Reserved[1..0] | INT | FAST | LOW)
-    Res2    = 0x02, // Reserved
-    Mode2   = 0x03, // Mode 2 register (T | LP | PT | Reserved[4..0])
+    Res = 0x00,   // Reserved
+    Mode1 = 0x01, // Mode 1 register (P | IICAddr[1..0] | Reserved[1..0] | INT | FAST | LOW)
+    Res2 = 0x02,  // Reserved
+    Mode2 = 0x03, // Mode 2 register (T | LP | PT | Reserved[4..0])
 }
 
 // TLV493D Measurement values
 #[derive(Debug, PartialEq, Clone)]
 pub struct Values {
-    x: f32,     // X axis magnetic flux (mT)
-    y: f32,     // Y axis magnetic flux (mT)
-    z: f32,     // Z axis magnetic flux (mT)
-    temp: f32,  // Device temperature (C)
+    x: f32,    // X axis magnetic flux (mT)
+    y: f32,    // Y axis magnetic flux (mT)
+    z: f32,    // Z axis magnetic flux (mT)
+    temp: f32, // Device temperature (C)
 }
 
 // Device operating mode
 // Note that in most cases the mode is a combination of mode and IRQ flags
 pub enum Mode {
-    Disabled,       // Reading disabled
-    Master,         // Master initiated mode (reading occurs after readout)
-    Fast,           // Fast mode (3.3kHz)
-    LowPower,       // Low power mode (100Hz)
-    UltraLowPower,  // Ultra low power mode (10Hz)
+    Disabled,      // Reading disabled
+    Master,        // Master initiated mode (reading occurs after readout)
+    Fast,          // Fast mode (3.3kHz)
+    LowPower,      // Low power mode (100Hz)
+    UltraLowPower, // Ultra low power mode (10Hz)
 }
 
 bitflags! {
@@ -104,27 +102,30 @@ bitflags! {
 
 /// Tlv493d related errors
 #[derive(Debug)]
-#[cfg_attr(feature = "std", derive(thiserror::Error))] 
+#[cfg_attr(feature = "std", derive(thiserror::Error))]
 pub enum Error<E: core::fmt::Debug> {
     // No device found with specified i2c bus and address
-    #[cfg_attr(feature = "std", error("No device found with specified i2c bus and address"))] 
+    #[cfg_attr(
+        feature = "std",
+        error("No device found with specified i2c bus and address")
+    )]
     NoDevice,
 
     // Device ADC locked up and must be reset
-    #[cfg_attr(feature = "std", error("Device ADC lockup, reset required"))] 
+    #[cfg_attr(feature = "std", error("Device ADC lockup, reset required"))]
     AdcLockup,
 
     // Uncorrect I2C address
-    #[cfg_attr(feature = "std", error("Sensor I2C address must be 0x5E or 0x1F"))]  /// Added ///
+    #[cfg_attr(feature = "std", error("Sensor I2C address must be 0x5E or 0x1F"))]
+    /// Added ///
     WrongI2CAddress,
 
     // Underlying I2C device error
-    #[cfg_attr(feature = "std", error("I2C device error: {0:?}"))] 
+    #[cfg_attr(feature = "std", error("I2C device error: {0:?}"))]
     I2c(E),
 }
 
-
-impl <I2c, E> Tlv493d<I2c, E>
+impl<I2c, E> Tlv493d<I2c, E>
 where
     I2c: i2c::Read<Error = E> + i2c::Write<Error = E> + i2c::WriteRead<Error = E>,
     E: core::fmt::Debug,
@@ -132,23 +133,32 @@ where
     /// Create a new TLV493D instance
     pub fn new(i2c: I2c, addr: u8, mode: &Mode) -> Result<Self, Error<E>> {
         debug!("New Tlv493d with address: 0x{:02x}", addr);
-        
+
         // Construct object
         let mut s = Self {
-            i2c, addr, initial: [0u8; 10], last_frm: 0xff, _e: PhantomData,
+            i2c,
+            addr,
+            initial: [0u8; 10],
+            last_frm: 0xff,
+            _e: PhantomData,
         };
 
         ////////////////////////// STARTUP PER FIG. 5.1 IN TLV493D-A1B6 USER MANUEL ///////////////////////
         // Write recovery value
-        let _= s.i2c.write(0xFF, &[]);
+        let _ = s.i2c.write(0xFF, &[]);
 
         // Send reset command
-        match s.addr {                                               //// Modified ////
-            ADDRESS_BASE_1 => { let _= s.i2c.write(0x00, &[0xFF]); } //The result is not used because it  will be an I2C error, because theses commands reset sensor and set-up
-            ADDRESS_BASE_2 => { let _= s.i2c.write(0x00, &[0x00]); } //its address, and the sensor do not respond with Ack-bit, so i2c method will interpret it as an error.
-            _other         => { return Err(Error::WrongI2CAddress) }
+        match s.addr {
+            //// Modified ////
+            ADDRESS_BASE_1 => {
+                let _ = s.i2c.write(0x00, &[0xFF]);
+            } //The result is not used because it  will be an I2C error, because theses commands reset sensor and set-up
+            ADDRESS_BASE_2 => {
+                let _ = s.i2c.write(0x00, &[0x00]);
+            } //its address, and the sensor do not respond with Ack-bit, so i2c method will interpret it as an error.
+            _other => return Err(Error::WrongI2CAddress),
         }
-       
+
         // Read initial bitmap from device
         let _ = s.i2c.read(s.addr, &mut s.initial[..]).map_err(Error::I2c)?;
         debug!("initial read: {:02x?}", s.initial);
@@ -175,18 +185,13 @@ where
 
         match mode {
             Mode::Disabled => (),
-            Mode::Master            => m1 |= Mode1::FAST | Mode1::LOW,
-            Mode::Fast              => m1 |= Mode1::FAST | Mode1::IRQ_EN,
-            Mode::LowPower          => m1 |= Mode1::LOW | Mode1::IRQ_EN,
-            Mode::UltraLowPower     => m1 |= Mode1::IRQ_EN,
+            Mode::Master => m1 |= Mode1::FAST | Mode1::LOW,
+            Mode::Fast => m1 |= Mode1::FAST | Mode1::IRQ_EN,
+            Mode::LowPower => m1 |= Mode1::LOW | Mode1::IRQ_EN,
+            Mode::UltraLowPower => m1 |= Mode1::IRQ_EN,
         }
 
-        let mut cfg = [
-            0x00,
-            m1.bits(),
-            self.initial[8],
-            m2.bits(),
-        ];
+        let mut cfg = [0x00, m1.bits(), self.initial[8], m2.bits()];
 
         let mut parity = 0;
         for v in &cfg {
@@ -208,7 +213,7 @@ where
 
     // Method to read raw values from the sensor
     // The sensor need to be already configured
-    //  INPUTS :    - 
+    //  INPUTS :    -
     //
     //  OUTPUTS :   - Result<[i16; 4], Error<E>>
     //                where [i16; 4] contains the values ==> Bx, By, Bz, Temp (no units)
@@ -223,11 +228,11 @@ where
         // Detect ADC lockup (stalled FRM field)
         let frm = b[3] & 0b0000_1100;
         if self.last_frm == frm {
-            return Err(Error::AdcLockup)
+            return Err(Error::AdcLockup);
         } else {
             self.last_frm = frm;
         }
-        
+
         // Convert to values (no units)
         // Double-cast here required for sign-extension
         v[0] = (b[0] as i8 as i16) << 4 | ((b[4] & 0xF0) >> 4) as i16;
@@ -240,15 +245,15 @@ where
         Ok(v)
     }
 
-
     // Read data and convert to correct values in mT and °C
     // The sensor need to be already configured
-    //  INPUTS :    - 
+    //  INPUTS :    -
     //
     //  OUTPUTS :   - Result<Values, Error<E>>
     //                where Values contains the values ==> Bx, By, Bz, Temp (with good units)
     //
-    pub fn read(&mut self) -> Result<Values, Error<E>> { //// Can not really be used because Values's field are private.... ////
+    pub fn read(&mut self) -> Result<Values, Error<E>> {
+        //// Can not really be used because Values's field are private.... ////
         let raw = self.read_raw()?;
         Ok(Values {
             x: raw[0] as f32 * 0.098f32,
@@ -258,12 +263,9 @@ where
         })
     }
 
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////
-//                                  ADDED BY ME
-///////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //                                  ADDED BY ME
+    ///////////////////////////////////////////////////////////////////////////////////////////
 
     // Method that read N times the magnetic field values on the sensor and return
     // a Bfield struct containing the mean of the N measurements.
@@ -272,39 +274,50 @@ where
     // INPUTS  : - n     : u16 (number of measurement on which we want to compute the mean)
     //           - delay : &mut rppal::hal::Delay (struct needed )
     //
-    // OUTPUTS : - Result --> Ok(Bfield struct), or Err(Error struct) 
+    // OUTPUTS : - Result --> Ok(Bfield struct), or Err(Error struct)
     //
-    pub fn get_b_mean(&mut self, n: u16, delay: &mut rppal::hal::Delay) -> Result<Bfield, Error<E>>  {
+    pub fn get_b_mean(
+        &mut self,
+        n: u16,
+        delay: &mut rppal::hal::Delay,
+    ) -> Result<Bfield, Error<E>> {
         //initialize b, and set counter to zero
-        let mut b = Bfield {bx:0.0, by:0.0, bz:0.0, ux:0.0, uy:0.0, uz:0.0};
+        let mut b = Bfield {
+            bx: 0.0,
+            by: 0.0,
+            bz: 0.0,
+            ux: 0.0,
+            uy: 0.0,
+            uz: 0.0,
+        };
         let mut j = 0; //counter of valid measurements
         let mut i = 0; //counter of invalid measurements (read_raw function returning an error )
 
         //These variables are needed to compte Mean and StandardDeviation iteratively
         let (mut bx_prev, mut by_prev, mut bz_prev): (f32, f32, f32);
-        
+
         //Looping until we get N valid measurements
-        while j!= n {
+        while j != n {
             (*delay).delay_us(400 as u16);
-            match self.read()  {
+            match self.read() {
                 Ok(b_new) => {
-                    j += 1;    //Iterating the number of valid measurements
+                    j += 1; //Iterating the number of valid measurements
 
                     bx_prev = b.bx; //
                     by_prev = b.by; //Storing previous means in 3 variables
                     bz_prev = b.bz; //
 
-                    b.bx = b.bx + (b_new.x - b.bx) / (j as f32) ; //
-                    b.by = b.by + (b_new.y - b.by) / (j as f32) ; //Compute new means
-                    b.bz = b.bz + (b_new.z - b.bz) / (j as f32) ; //
+                    b.bx = b.bx + (b_new.x - b.bx) / (j as f32); //
+                    b.by = b.by + (b_new.y - b.by) / (j as f32); //Compute new means
+                    b.bz = b.bz + (b_new.z - b.bz) / (j as f32); //
 
-                    b.ux = b.ux + (b_new.x - bx_prev) * (b_new.x - b.bx) ; //
-                    b.uy = b.uy + (b_new.y - by_prev) * (b_new.y - b.by) ; //Compute new variances
-                    b.uz = b.uz + (b_new.z - bz_prev) * (b_new.z - b.bz) ; //
+                    b.ux = b.ux + (b_new.x - bx_prev) * (b_new.x - b.bx); //
+                    b.uy = b.uy + (b_new.y - by_prev) * (b_new.y - b.by); //Compute new variances
+                    b.uz = b.uz + (b_new.z - bz_prev) * (b_new.z - b.bz); //
                 }
                 Err(error) => {
-                    i += 1;   //Iterating the number of invalid mesurements
-                    if i > n { 
+                    i += 1; //Iterating the number of invalid mesurements
+                    if i > n {
                         return Err(error); // If there are too many errors while trying to do the N measurements, return error
                     }
                 }
@@ -317,45 +330,43 @@ where
         Ok(b) //Returning Ok(b) if the n measurements are made correctly
     }
 
-
     // Function that read N times the magnetic field values on the sensor, average them, and then
     // return a tuple containing the angle between Bxy and X axis, and its standard deviation.
     //
     // INPUTS  : - n     : u16 (number of measurement on which we want to compute the mean)
     //           - delay : &mut rppal::hal::Delay (struct needed )
     //
-    // OUTPUTS : - Result --> Ok(Bfield struct), or Err(Error struct) 
-    // 
-    pub fn get_bxy_angle(&mut self, n: u16, delay: &mut rppal::hal::Delay) -> Result<(f32, f32), Error<E>>  {
-        let angle_xy:   f32;
+    // OUTPUTS : - Result --> Ok(Bfield struct), or Err(Error struct)
+    //
+    pub fn get_bxy_angle(
+        &mut self,
+        n: u16,
+        delay: &mut rppal::hal::Delay,
+    ) -> Result<(f32, f32), Error<E>> {
+        let angle_xy: f32;
         let u_angle_xy: f32;
         match self.get_b_mean(n, delay) {
             Ok(b) => {
                 (angle_xy, u_angle_xy) = b.b_angle_xy();
                 Ok((angle_xy, u_angle_xy))
             }
-            Err(error) => { return Err(error)}
+            Err(error) => return Err(error),
         }
     }
-
 }
-
-
 
 // Struct that contains the 3 components of the magnetic field, in mT, and their standard-deviations (>0 if several measurements are made)
 // "Values" already exists but does not include standard-deviation. A little bit redundant...
 pub struct Bfield {
-    pub bx : f32, // X axis field compenent
-    pub by : f32, // Y axis field component
-    pub bz : f32, // Z axis field component
-    pub ux : f32, // Standard deviation for X axis compenent
-    pub uy : f32, // Standard deviation for X axis compenent
-    pub uz : f32, // Standard deviation for Z axis component
+    pub bx: f32, // X axis field compenent
+    pub by: f32, // Y axis field component
+    pub bz: f32, // Z axis field component
+    pub ux: f32, // Standard deviation for X axis compenent
+    pub uy: f32, // Standard deviation for X axis compenent
+    pub uz: f32, // Standard deviation for Z axis component
 }
 
-
 impl Bfield {
-
     // Function that compute the angle made by the magnetic field on the XY plan based on
     // a N-measurements mean value of the magnetic field
     //
@@ -365,21 +376,19 @@ impl Bfield {
     //                ---> 0.0 to 360.0
     //           - u_angle_xy : f32 (computed standard-deviation on angle_xy, in degrees °)
     //
-    pub fn b_angle_xy(& self) -> (f32,f32) 
-    {
+    pub fn b_angle_xy(&self) -> (f32, f32) {
         let b = self;
         //Compute angle (four quadrant arctangent of By/Bx ), and its standard-deviation.
         let theta = b.by.atan2(b.bx) * 180.0 / 3.1415;
 
-        let u_sensor:f32 = 0.01;         //uncertainty due to sensor accuracy (0.1mT/LSB, and mean over 150 points)
-        let u_hysteresis:f32 = 0.25;    //uncertainty due to hysteresis (measured on our sensor)
-        let u_bi:f32 = (u_sensor.powi(2) + u_hysteresis.powi(2)).sqrt(); //uncertainty on Bx and on By.
+        let u_sensor: f32 = 0.01; //uncertainty due to sensor accuracy (0.1mT/LSB, and mean over 150 points)
+        let u_hysteresis: f32 = 0.25; //uncertainty due to hysteresis (measured on our sensor)
+        let u_bi: f32 = (u_sensor.powi(2) + u_hysteresis.powi(2)).sqrt(); //uncertainty on Bx and on By.
 
-        let dbx:f32 = -b.by / ( b.bx.powi(2) + b.by.powi(2) ); //derivative of Theta / bx
-        let dby:f32 = b.bx / ( b.bx.powi(2) + b.by.powi(2) ); //derivative of Theta / by
+        let dbx: f32 = -b.by / (b.bx.powi(2) + b.by.powi(2)); //derivative of Theta / bx
+        let dby: f32 = b.bx / (b.bx.powi(2) + b.by.powi(2)); //derivative of Theta / by
 
-        let u_theta:f32 = u_bi * ( dbx.powi(2) + dby.powi(2) ).sqrt() /3.1415*180.0;
+        let u_theta: f32 = u_bi * (dbx.powi(2) + dby.powi(2)).sqrt() / 3.1415 * 180.0;
         (theta, u_theta)
     }
-
 }
